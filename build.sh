4d858e23bca050534bf754c769974cb1a3e6ed91
Artem Smirnov <artsmi.vic@gmail.com>
2024-12-21 14:11:32 +0500

    feat: fix build.sh - add oprion for auto creation directory On branch develop

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cd build
+
+BUILD_DIRECTORY="./build"
+
+if [ -d "$BUILD_DIRECTORY" ]; then
+  echo "directory $BUILD_DIRECTORY does exist..."
+else
+  mkdir $BUILD_DIRECTORY
+  echo "directory $BUILD_DIRECTORY was created..."
+fi
+
+cd $BUILD_DIRECTORY
 cmake ..
 make
