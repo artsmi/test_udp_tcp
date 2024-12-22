26eb2cbdb538b08e24eeeb48503c595b94d5e052
Artem Smirnov <artsmi.vic@gmail.com>
2024-12-22 15:25:38 +0500

    feat: cppcheck is for only c files On branch develop

--- check_format.sh
+++ check_format.sh
@@ -6,13 +6,11 @@ NORMAL='\033[0m'
 DEF='\033[0;39m'
 LBLUE='\033[1;34m'
 
-FIND_IGNORE_PATHS = -not -path './install*' -not -path './build*'
-
 echo -en "\n${LBLUE}${BOLD} CPPCHECK is starting...\n ${DEF}${NORMAL}\n"
 find . \
   -not -path './install*'\
   -not -path './build*'\
-  -regex '.*/.*\.\(c\|cpp\|h\)$' \
+  -regex '.*/.*\.\(c\)$' \
   -exec cppcheck -v --enable=all\
   --suppress=missingIncludeSystem\
   --suppress=checkersReport\
