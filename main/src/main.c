39aa8a58a4d9d56f8e6d2487bbd84d700f206b6b
Artem Smirnov <artsmi.vic@gmail.com>
2024-12-23 20:22:20 +0500

    chore: create review version On branch review

--- main/src/main.c
+++ main/src/main.c
@@ -22,38 +22,13 @@ int main(int argc, char* argv[]) {
 
   TCP_UDP_DATA data;
   tud_prepare(&data);
-
   thm_prepare(&gtmgr);
-  thm_add_thread(&gtmgr, "SimpleSample", &simple_sample_start, NULL);
 
-  THREAD_SETTINGS* pts_tcpserver =
-      thm_add_thread(&gtmgr, "(S)TCP_Server", &tcp_server_start, NULL);
-  THREAD_SETTINGS* pts_tcpclient =
-      thm_add_thread(&gtmgr, "(S)TCP_Client", &tcp_client_start, (void*)&data);
-
-  thm_add_thread(&gtmgr, "(C)UDP_Pusher", &udp_client_pusher_start, NULL);
+  thm_add_thread(&gtmgr, "(S)TCP_Client", &tcp_client_start, (void*)&data);
   thm_add_thread(&gtmgr, "(S)UDP_Server", &udp_server_start, (void*)&data);
 
   signal(SIGINT, interrupt_function);
   signal(SIGKILL, interrupt_function);
-
-  usleep(3000000);
-  ulog(LL_I, "Crashing TCP Client...");
-  ut_cancel(pts_tcpclient);
-  ut_join(pts_tcpclient);
-
-  usleep(3000000);
-  ulog(LL_I, "Starting TCP Client...");
-  thm_add_thread(&gtmgr, "(S)TCP_Client 2", &tcp_client_start, (void*)&data);
-
-  usleep(3000000);
-  ulog(LL_I, "Crashing TCP Server...");
-  ut_cancel(pts_tcpserver);
-  ut_join(pts_tcpserver);
-
-  usleep(3000000);
-  ulog(LL_I, "Starting TCP Server...");
-  thm_add_thread(&gtmgr, "(S)TCP_Server 2", &tcp_server_start, NULL);
 
   thm_wait_all(&gtmgr);
   thm_destroy_all(&gtmgr);
